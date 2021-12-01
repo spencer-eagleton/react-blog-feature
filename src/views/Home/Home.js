70aa94fd8cf13d8f044f229367541bd4c9add24b
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-01 14:26:57 -0800

    default to center

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
   const [title, setTitle] = useState('');
   const [subtitle, setSubtitle] = useState('');
   const [font, setFont] = useState('');
-  const [align, setAlign] = useState('');
+  const [align, setAlign] = useState('center');
   const [text, setText] = useState('');
   // add useState calls here for title, subtitle, font, align, and text
 
