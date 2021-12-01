64c8486c02cb360b37e89bd3e934f7feb34fd338
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-12-01 12:09:22 -0800

    added setTitle

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 
 import Preview from '../../components/Preview/Preview';
 import Editor from '../../components/Editor/Editor';
@@ -6,13 +6,14 @@ import Editor from '../../components/Editor/Editor';
 import './Home.css';
 
 export default function Home() {
+  const [title, setTitle] = useState('');
   // add useState calls here for title, subtitle, font, align, and text
 
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview />
-      <Editor />
+      <Preview {...{ title, setTitle }} />
+      <Editor {...{ title, setTitle }} />
     </main>
   );
 }
